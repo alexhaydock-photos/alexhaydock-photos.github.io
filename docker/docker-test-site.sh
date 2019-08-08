4b69173cbf20734e9e1b482f858e915e3cd3125d
Alex Haydock <alex@alexhaydock.co.uk>
2019-08-08 20:08:08 +0100

    Update images

--- docker/docker-test-site.sh
+++ docker/docker-test-site.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
-set -xe
+set -x
 if [ "$EUID" -ne 0 ]; then echo "This script requires root to issue Docker commands."; exit; fi
+
+docker rm alexhaydock-photos
 
 docker build -t alexhaydock-photos .
 
 docker run --rm -it \
   --name "alexhaydock" \
-  -v "/home/a/gitlab/photos.alexhaydock.co.uk:/opt/www" \
+  -v "$(pwd)/../:/opt/www/" \
   -p "127.0.0.1:4000:4000/tcp" \
   alexhaydock-photos \
     bundle exec jekyll serve --incremental -H 0.0.0.0
